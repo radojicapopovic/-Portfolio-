d25bb7fa60d2c4ec8791e08dcbba37cf8666a629
Radojica Popovc <108525632+radojicapopovic@users.noreply.github.com>
2022-12-08 02:25:51 +0100

    Update Skill.tsx

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { Skill as TypeSkill } from "../typings";
-import { urlFor } from "../sanity";
+// import { urlFor } from "../sanity";
 
 interface Props {
   skill: TypeSkill;
@@ -26,7 +26,7 @@ const Skill = ({ skill, directionLeft }: Props) => {
         viewport={{
           once: true,
         }}
-        src={urlFor(skill?.image).url()}
+        // src={urlFor(skill?.image).url()}
         className="rounded-full border border-gray-500 object-cover w-24 h-24 filter group-hover:grayscale transition duration-300 ease-in-out"
       />
       <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 rounded-full z-0">
